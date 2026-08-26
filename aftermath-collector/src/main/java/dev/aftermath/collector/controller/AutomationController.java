2d60a108905f70482088e2a607c364d3d9cd6129
Anshuman singh <anshumansingh5438@gmail.com>
2026-08-26 14:01:36 +0530

    refactor: revert automatic Git PR pushing code and enforce AFTERMATH Default local-first architecture

--- aftermath-collector/src/main/java/dev/aftermath/collector/controller/AutomationController.java
+++ aftermath-collector/src/main/java/dev/aftermath/collector/controller/AutomationController.java
@@ -2,8 +2,6 @@ package dev.aftermath.collector.controller;
 
 import dev.aftermath.collector.service.ContractTestGenerator;
 import dev.aftermath.collector.service.ContractTestGenerator.ContractTestArtifact;
-import dev.aftermath.collector.service.GitPrBotService;
-import dev.aftermath.collector.service.GitPrBotService.GitPrPayload;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,14 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class AutomationController {
 
-    private final GitPrBotService gitPrBotService;
     private final ContractTestGenerator contractTestGenerator;
-
-    @GetMapping("/incidents/{incidentId}/git-pr-payload")
-    public ResponseEntity<GitPrPayload> getGitPrPayload(@PathVariable(name = "incidentId") String incidentId) {
-        GitPrPayload payload = gitPrBotService.generatePrPayload(incidentId);
-        return ResponseEntity.ok(payload);
-    }
 
     @GetMapping("/incidents/{incidentId}/contract-test")
     public ResponseEntity<ContractTestArtifact> getContractTest(@PathVariable(name = "incidentId") String incidentId) {
